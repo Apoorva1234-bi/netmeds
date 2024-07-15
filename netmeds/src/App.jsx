7ef14f0ede35d9858f8ec52a22f1096a67b20363
Apoorva Narnaware <apoorvanarnaware@gmail.com>
2024-07-15 20:48:55 +0530

    Boiler Plate added

---
+++ netmeds/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Home_Body from './pages/Home_Body';
+
+
+
+function App() {
+
+
+  return (
+      <div id="Home_Content">
+         <Home_Body/>
+      </div>
+  )
+}
+
+export default App;
---
+++ netmeds/src/pages/Offers.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Offers =()=>{
+   return(
+    <div>
+        <h1>This is Offers Page</h1>
+    </div>
+   )
+}
+export default Offers;
