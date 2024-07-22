3daf6073563ba7aa4a025be09507754cf7abed27
komalsatale <123380647+komalsatale@users.noreply.github.com>
2024-07-22 18:22:04 +0530

    one file is added for trying

---
+++ netmeds/src/pages/HomeFitnessDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomeFitnessDetails(props) {
+    return (
+        <div>
+            <h3>Fitness Details page</h3>
+        </div>
+    );
+}
+
+export default HomeFitnessDetails;
